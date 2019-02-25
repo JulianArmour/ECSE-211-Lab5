89a9bfce34609a2a06b84f4311df5a7a004f72eb
Julian Armour <julian.armour@videotron.ca>
2019-02-25 12:35:48 -0500

    skipping if statement

--- src/ca/mcgill/ecse211/lab5/Lab5.java
+++ src/ca/mcgill/ecse211/lab5/Lab5.java
@@ -33,25 +33,25 @@ public class Lab5 {
     private static final int URy = 7;
     private static final int SC = 0;
     private static final int TR = 0;
-    
+
     // physical values for LLx, LLy, URx, URy
     private static double PLLx;
     private static double PLLy;
     private static double PURx;
     private static double PURy;
-    
+
     private static final int CAN_COLOR = 0;
 
-	private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("C"));
-	private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
-	private static final TextLCD lcd = LocalEV3.get().getTextLCD();
+    private static final EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("C"));
+    private static final EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("D"));
+    private static final TextLCD lcd = LocalEV3.get().getTextLCD();
 
-	/** The tile's length. */
+    /** The tile's length. */
     public static final double TILE_SIZE = 30.48;
-	public static final double WHEEL_RAD = 2.2;
-	public static final double TRACK = 11.75;
-	public static boolean wall;
-	
+    public static final double WHEEL_RAD = 2.2;
+    public static final double TRACK = 11.75;
+    public static boolean wall;
+
     private static MovementController movementController;
     private static LLnavigator llNavigator;
     private static Port sideUSPort;
@@ -71,7 +71,6 @@ public class Lab5 {
     private static SensorMode sideLSProvider;
     private static float[] sideLSSample;
     private static float[][] arrayColor;
-    
 
     private static Odometer odometer;
     private static DifferentialLightSensor leftDifferentialLightSensor;
@@ -86,113 +85,116 @@ public class Lab5 {
     private static ColourLightSensor colourLightSensor;
     private static URnavigator urNavigator;
 
-	public static void main(String[] args) throws OdometerExceptions {
-		int buttonChoice;
-		
-		// coordinate conversion
-		PLLx = TILE_SIZE * (double) LLx;
-		PLLy = TILE_SIZE * (double) LLy;
-		PURx = TILE_SIZE * (double) URx;
-		PURy = TILE_SIZE * (double) URy;
-		
-		// set up side ultrasonic sensor
+    public static void main(String[] args) throws OdometerExceptions {
+        int buttonChoice;
+
+        // coordinate conversion
+        PLLx = TILE_SIZE * (double) LLx;
+        PLLy = TILE_SIZE * (double) LLy;
+        PURx = TILE_SIZE * (double) URx;
+        PURy = TILE_SIZE * (double) URy;
+
+        // set up side ultrasonic sensor
         sideUSPort = LocalEV3.get().getPort("S1");
         sideUltrasonicSensor = new EV3UltrasonicSensor(sideUSPort);
         sideDistanceProvider = sideUltrasonicSensor.getMode("Distance");
         sideUSSample = new float[sideDistanceProvider.sampleSize()];
-        
+
         // set up back-left light sensor
         backLeftLSPort = LocalEV3.get().getPort("S2");
         backLeftLS = new EV3ColorSensor(backLeftLSPort);
         backLeftLSProvider = backLeftLS.getMode("Red");
         backLeftLSSample = new float[backLeftLSProvider.sampleSize()];
-        
+
         // set up back-right light sensor
         backRightLSPort = LocalEV3.get().getPort("S3");
         backRightLS = new EV3ColorSensor(backRightLSPort);
         backRightLSProvider = backRightLS.getMode("Red");
         backRightLSSample = new float[backRightLSProvider.sampleSize()];
-        
+
         // set up side light sensor
         sideLSPort = LocalEV3.get().getPort("S4");
-        sideLS= new EV3ColorSensor(sideLSPort);
+        sideLS = new EV3ColorSensor(sideLSPort);
         sideLSProvider = sideLS.getMode("RGB");
         sideLSSample = new float[sideLSProvider.sampleSize()];
-        
-        
+
         odometer = Odometer.getOdometer(leftMotor, rightMotor, TRACK, WHEEL_RAD);
-        Thread odoThread= new Thread(odometer);
+        Thread odoThread = new Thread(odometer);
         odoThread.start();
-        
+
         movementController = new MovementController(leftMotor, rightMotor, WHEEL_RAD, TRACK, odometer);
-        
+
         leftDifferentialLightSensor = new DifferentialLightSensor(backLeftLSProvider, backLeftLSSample);
         rightDifferentialLightSensor = new DifferentialLightSensor(backRightLSProvider, backRightLSSample);
-        
-        
+
         angleCorrection = new angleCorrection(rightDifferentialLightSensor, leftDifferentialLightSensor,
-                                              movementController, odometer);
+                movementController, odometer);
         colourLightSensor = new ColourLightSensor(sideLSProvider, sideLSSample);
         medSensor = new MedianDistanceSensor(sideDistanceProvider, sideUSSample, odometer);
         circleFollow = new CircleFollow(movementController, odometer, medSensor, colourLightSensor, CAN_COLOR);
         usAngleCorrector = new USAngleCorrector(movementController, odometer, medSensor);
-        axesLocalizer = new AxesLocalizer(movementController, odometer, leftDifferentialLightSensor, rightDifferentialLightSensor);
+        axesLocalizer = new AxesLocalizer(movementController, odometer, leftDifferentialLightSensor,
+                rightDifferentialLightSensor);
         intersectionLocalizer = new IntersectionLocalizer(leftDifferentialLightSensor, movementController, odometer);
-        searchNavigator = new SearchNavigator(odometer, movementController, LLx, LLy, URx, URy, medSensor, circleFollow, angleCorrection);
+        searchNavigator = new SearchNavigator(odometer, movementController, LLx, LLy, URx, URy, medSensor, circleFollow,
+                angleCorrection);
         urNavigator = new URnavigator(URy, URx, movementController, odometer);
 
+        do {
+            lcd.clear();
+            lcd.drawString("Press left for routine", 0, 0);
+            lcd.drawString("Press right for color", 0, 1);
+            buttonChoice = Button.waitForAnyPress();
+        } while (buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT);
         
-		do {
-			lcd.clear();
-			lcd.drawString("Press up for routine", 0, 0);
-			lcd.drawString("Press down for color", 0, 1);
-			buttonChoice = Button.waitForAnyPress();
-		} while (buttonChoice != Button.ID_UP && buttonChoice != Button.ID_DOWN);
-		
-		
-		if (buttonChoice == Button.ID_UP) {
-			lcd.clear();
-			 /**usAngleCorrector.fallingEdge();
-             axesLocalizer.estimatePosition();
-             axesLocalizer.travelCloseToOrigin();
-             intersectionLocalizer.getIntersections();
-             intersectionLocalizer.correctAngle();
-             intersectionLocalizer.correctPosition();
-             movementController.travelTo(0, 0, false);
-             movementController.turnTo(0);**/
-			
-             llNavigator.navigateToLL();
-             
-             searchNavigator.searchPath();
-             
-             urNavigator.navigateToUr();
-             
-		}else {
-			arrayColor = new float[100][3];
-			if(sideLSSample[0] >= (0.00098) && sideLSSample[1] >= (0.00098) && sideLSSample[2] >= (0.00098)) {
-				lcd.drawString("Object detected", 0, 1);
-				for(int i = 0; i < 100; i++) {
+        System.out.println(buttonChoice);
 
-					sideLSProvider.fetchSample(sideLSSample, 0);
+        if (buttonChoice == Button.ID_LEFT) {
+            System.out.println("going up");
+            lcd.clear();
+            /**
+             * usAngleCorrector.fallingEdge(); axesLocalizer.estimatePosition();
+             * axesLocalizer.travelCloseToOrigin();
+             * intersectionLocalizer.getIntersections();
+             * intersectionLocalizer.correctAngle();
+             * intersectionLocalizer.correctPosition(); movementController.travelTo(0, 0,
+             * false); movementController.turnTo(0);
+             **/
 
-					if(sideLSSample[0] <= (0.00098) && sideLSSample[1] <= (0.00098) && sideLSSample[2] <= (0.00098)) {
-						i--;
-					}else {
-						arrayColor[i] = sideLSSample;
-					}
-					try {
-						Thread.sleep(75);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				System.out.println("Can: " + ColorDetector.verifyCan(arrayColor, 4));
-			}
-		}
+            llNavigator.navigateToLL();
 
-		while (Button.waitForAnyPress() != Button.ID_ESCAPE);
-		System.exit(0);
-	}
+            searchNavigator.searchPath();
+
+            urNavigator.navigateToUr();
+
+        } else if (buttonChoice == Button.ID_RIGHT) {
+            System.out.println("going down");
+            arrayColor = new float[100][3];
+            if (sideLSSample[0] >= (0.00098) && sideLSSample[1] >= (0.00098) && sideLSSample[2] >= (0.00098)) {
+                lcd.drawString("Object detected", 0, 1);
+                for (int i = 0; i < 100; i++) {
+
+                    sideLSProvider.fetchSample(sideLSSample, 0);
+
+                    if (sideLSSample[0] <= (0.00098) && sideLSSample[1] <= (0.00098) && sideLSSample[2] <= (0.00098)) {
+                        i--;
+                    } else {
+                        arrayColor[i] = sideLSSample;
+                    }
+                    try {
+                        Thread.sleep(75);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Can: " + ColorDetector.verifyCan(arrayColor, 4));
+            }
+        }
+
+        System.out.println("Big wtf");
+        while (Button.waitForAnyPress() != Button.ID_ESCAPE)
+            ;
+        System.exit(0);
+    }
 
 }
